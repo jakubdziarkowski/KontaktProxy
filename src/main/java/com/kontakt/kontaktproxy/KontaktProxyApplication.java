c4c109b56c514abd7d46760f3076d762a5d9be1e
Jakub Dziarkowski <jakub.dziarkowski@bimodal-solutions.com>
2024-01-21 10:26:56 +0100

    Initial commit

---
+++ src/main/java/com/kontakt/kontaktproxy/KontaktProxyApplication.java
@@ -0,0 +1,13 @@
+package com.kontakt.kontaktproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KontaktProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KontaktProxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kontakt/kontaktproxy/KontaktProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kontakt.kontaktproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KontaktProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
