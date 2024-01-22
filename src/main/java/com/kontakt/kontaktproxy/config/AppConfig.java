a83246da3885a134ff6500d5e56dc228f419e267
Jakub Dziarkowski <jakub.dziarkowski@bimodal-solutions.com>
2024-01-22 14:07:59 +0100

    remove unused bean

--- src/main/java/com/kontakt/kontaktproxy/config/AppConfig.java
+++ src/main/java/com/kontakt/kontaktproxy/config/AppConfig.java
@@ -1,12 +1,8 @@
 package com.kontakt.kontaktproxy.config;
 
-import io.github.bucket4j.Bucket;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Configuration
 public class AppConfig {
@@ -14,10 +10,5 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
-    }
-
-    @Bean
-    public Map<String, Bucket> rateLimits() {
-        return new ConcurrentHashMap<>();
     }
 }
